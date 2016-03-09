aa66e64806efcc3fc33358e0836e9c11c5f8d1fc
s0248 <kjj3300@163.com>
2016-03-09 22:16:08 +0800

    clean

--- labcodes/lab2/kern/mm/pmm.c
+++ labcodes/lab2/kern/mm/pmm.c
@@ -447,17 +447,6 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
     	tlb_invalidate(pgdir, la);
 
     }
-
-    //    if ((* ptep) & PTE_P) {
-//        struct Page * page = pte2page(*ptep);
-//        page_ref_dec(page);
-//        if (page->ref == 0) free_page(page);
-//        *ptep = 0;
-//        tlb_invalidate(pgdir, la);
-//    }
-
-
-
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
